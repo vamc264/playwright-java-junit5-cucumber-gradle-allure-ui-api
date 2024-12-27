b2e990f2e0fd052073f1aa0420060534b6c809d6
vamc264 <vamc264@gmail.com>
2024-12-27 22:33:58 +0000

    updated scenario with accessibility’s code using axe-core

--- src/test/java/stepdefinitions/GoogleSearchSteps.java
+++ src/test/java/stepdefinitions/GoogleSearchSteps.java
@@ -4,23 +4,22 @@ import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import utils.PlaywrightManager;
 import io.cucumber.java.en.*;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GoogleSearchSteps {
     private Page page;
-    private  BrowserType[] browsersToTest = PlaywrightManager.multipleBrowser();
 
-    @Given("I open the Google homepage")
-    public void iOpenTheGoogleHomepage() {
-        for (int i = 0; i < browsersToTest.length; i++) {
-            PlaywrightManager.setup(browsersToTest[i]);
-            page = PlaywrightManager.getPage();
-            page.navigate("https://www.google.com");
-        }
+    @Given("I open the Google homepage in {string}")
+    public void iOpenTheGoogleHomepageInBrowserName(String browserName) {
+        PlaywrightManager.multipleBrowserSetup(browserName);
+        page = PlaywrightManager.getPage();
+        page.navigate("https://www.google.com");
     }
 
     @When("I search for {string}")
     public void iSearchFor(String searchTerm) {
+        page = PlaywrightManager.getPage();
         page.click("//*[@id=\"W0wltc\"]");
         page.click("//*[@id=\"APjFqb\"]");
         page.fill("//*[@id=\"APjFqb\"]", searchTerm);
@@ -36,15 +35,6 @@ public class GoogleSearchSteps {
         } catch (AssertionError e) {
             PlaywrightManager.captureScreenshot("GoogleSearchFailure.png");
             throw e;
-        } finally {
-            PlaywrightManager.tearDown();
         }
-    }
-
-    @Given("I open the Google homepage in {string}")
-    public void iOpenTheGoogleHomepageInBrowserName(String browserName) {
-        PlaywrightManager.multipleBrowserSetup(browserName);
-        page = PlaywrightManager.getPage();
-        page.navigate("https://www.google.com");
     }
 }
